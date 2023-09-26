e95a24d54a9d2f5d6be87064ee929146a16b445f
Juho Björkman <juho.bjorkman@tuni.fi>
2023-09-26 23:17:33 +0300

    #20: Add content to roles table in migration script

--- backend/db/migrations/20230926173433_add_role_to_user.sql
+++ backend/db/migrations/20230926173433_add_role_to_user.sql
@@ -5,6 +5,8 @@ CREATE TABLE `roles` (
   PRIMARY KEY (`id`)
 );
 
+INSERT INTO `roles`(`name`) VALUES ('user'),('caretaker'),('admin');
+
 ALTER TABLE `users` ADD COLUMN `role_id` INT NOT NULL;
 
 ALTER TABLE `users`
