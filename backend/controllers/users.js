5a2c58d65f0ac545aca50717a24bcdd7a98b55c5
Juho Björkman <juho.bjorkman@tuni.fi>
2023-09-29 01:01:45 +0300

    Implement login API endpoint

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -57,4 +57,74 @@ const signup = async (req, res) => {
   }
 };
 
-module.exports = signup;
+const login = async (req, res) => {
+  const schema = Joi.object({
+    email: Joi.string().email().required(),
+    password: Joi.string()
+      .pattern(
+        new RegExp("^(?=.*?[a-z])(?=.*?[A-Z])(?=.*?[0-9])[a-zA-Z].{8,}$")
+      )
+      .required()
+  });
+
+  const providedCredentials = {
+    email: req.body.email,
+    password: req.body.password
+  };
+
+  try {
+    const { error } = schema.validate(providedCredentials);
+    if (error) throw error;
+  } catch (error) {
+    return res.status(400).send(error.details[0].message);
+  }
+
+  // Check that a user with the email exists.
+  let users;
+  try {
+    users = await userModels.findByEmail(providedCredentials.email);
+    if (users.length === 0) {
+      throw new Error("No user exists for given email");
+    }
+  } catch (error) {
+    return res.status(401).send(error.message);
+  }
+
+  const passwordsMatch = await bcrypt.compare(
+    providedCredentials.password,
+    users[0].password
+  );
+
+  if (!passwordsMatch) {
+    return res
+      .status(401)
+      .send(
+        "Invalid credentials. Please check email and password and try again."
+      );
+  }
+
+  const authenticatedUser = {
+    id: users[0].id,
+    role: users[0].role,
+    firstname: users[0].first_name,
+    lastname: users[0].last_name,
+    email: users[0].email,
+    postalcode: users[0].postal_code,
+    city: users[0].city,
+    country: users[0].country,
+    phone: users[0].phone,
+    premium: users[0].premium
+  };
+
+  try {
+    jwt.sign(authenticatedUser, process.env.JWT_KEY, (error, token) => {
+      if (error) throw error;
+      authenticatedUser.token = token;
+      res.send(authenticatedUser);
+    });
+  } catch (error) {
+    return res.status(500).send(error.message);
+  }
+};
+
+module.exports = { signup, login };
--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,12 +1,10 @@
 const express = require("express");
 const router = express.Router();
 
-const signup = require("../controllers/users");
+const { signup, login } = require("../controllers/users");
 
 router.post("/signup", signup);
 
-router.post("/login", (req, res) => {
-  res.send("The login endpoint");
-});
+router.post("/login", login);
 
 module.exports = router;
