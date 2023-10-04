14e4232f59c9f031533ae337955c82ff98f3e941
Juho Björkman <juho.bjorkman@tuni.fi>
2023-10-04 22:24:49 +0300

    Implement check administrator role middleware #28

--- backend/middleware/checkAdministrator.js
+++ backend/middleware/checkAdministrator.js
@@ -1,5 +1,21 @@
 const checkAdministrator = (req, res, next) => {
-  console.log("checkAdministrator middleware");
+  if (req.method === "OPTIONS") {
+    next();
+  }
+
+  // Prevent missing request body from crashing the app.
+  try {
+    if (!req.body.role_id) {
+      throw new Error("No role ID was provided");
+    }
+  } catch (error) {
+    return res.status(401).send("No role ID was provided");
+  }
+
+  if (req.body.role_id < 3) {
+    return res.status(401).send("Insufficient account role");
+  }
+  next();
 };
 
 module.exports = checkAdministrator;
