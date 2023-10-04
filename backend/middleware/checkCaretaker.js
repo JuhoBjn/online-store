0947fe642cf1d299f7fa0e3f5053cf38f2d17bdc
Juho Björkman <juho.bjorkman@tuni.fi>
2023-10-04 21:58:36 +0300

    Implement middleware to check if user is a caretaker #28

--- backend/middleware/checkCaretaker.js
+++ backend/middleware/checkCaretaker.js
@@ -1,5 +1,12 @@
 const checkCaretaker = (req, res, next) => {
-  console.log("checkCaretaker middleware.");
+  if (req.method === "OPTIONS") {
+    next();
+  }
+
+  if (req.body.role_id === 1) {
+    return res.status(401).send("Insufficient account role");
+  }
+  next();
 };
 
 module.exports = checkCaretaker;
