ad389bd91774d4a73a2e9398a50d61fa70a286dd
Valtteri Huuskonen <valtteri.huuskonen@tuni.fi>
2023-10-06 02:31:05 +0300

    Add a test user with a known password

--- backend/db/testdata/file.sql
+++ backend/db/testdata/file.sql
@@ -2,7 +2,8 @@
 
 -- Users table
 -- Creates four test users, two regular user, one caretaker and one admin.
--- Regular premium user.
+--
+-- Regular premium user
 INSERT INTO `users`(`id`,`first_name`,`last_name`,`email`,`postal_code`,`city`,`country`,`phone`,`password`,`premium`,`role_id`)
 SELECT '858560f9-fc03-43b0-b931-01213e4787ce','Lotta','Schmiedmann','lottas@test.com','65300','Vaasa','fi','2589631470','$2y$12$Z1PD1PxuAyVn0IgKp.hOMu1C/iPLiGmVB0rcljJYVPPIST9sf/Ql.',1,id FROM `roles` WHERE `name`='user';
 
@@ -17,3 +18,7 @@ SELECT '59158c35-8d77-43f1-bc63-3c5b4265b276','Bob','Builer','bobb@test.com','00
 -- Admin, premium user
 INSERT INTO `users`(`id`,`first_name`,`last_name`,`email`,`postal_code`,`city`,`country`,`phone`,`password`,`premium`,`role_id`)
 SELECT 'e16a6eac-9993-4137-9221-7c879337bbe4','Anthony','Administrator','anthonya@test.com','20500','Turku','fi','1397824650','$2y$12$X0ByuB55Ca.59od3gFL2/eSZs5Zx9ZWmFGtq0cIIJKpNwYWYYi5Jy',1,id FROM `roles` WHERE `name`='admin';
+
+-- Admin, non-premium user. Password: testPassw0rd
+INSERT INTO `users`(`id`,`first_name`,`last_name`,`email`,`postal_code`,`city`,`country`,`phone`,`password`,`premium`,`role_id`)
+SELECT '41284e8d-ee2b-4e15-b9af-296de2a9af8a','Adam','Administrator','adama@test.com','57710','Savonlinna','fi','+358123456790','$2a$12$GVly7eDkfYYbsJZTy9ug7.ZMLxgtXw7IcuVS2vMpG1UdnZq7mNvCq',0,id FROM `roles` WHERE `name`='admin';
