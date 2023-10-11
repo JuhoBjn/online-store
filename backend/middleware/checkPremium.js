ee863eeb1bed79995b4d6d9443eaac3a9a10b7ae
Juho Björkman <juho.bjorkman@tuni.fi>
2023-10-11 15:50:22 +0300

    Add middleware to check a user's premium status #38

--- backend/middleware/checkPremium.js
+++ backend/middleware/checkPremium.js
@@ -1,3 +1,20 @@
-const checkPremium = (req, res, next) => {};
+const checkPremium = (req, res, next) => {
+  if (req.method === "OPTIONS") {
+    return next();
+  }
+
+  try {
+    if (req.body.premium === undefined) {
+      throw new Error("No premium was provided");
+    }
+  } catch (error) {
+    return res.status(400).send("No premium was provided");
+  }
+
+  if (req.body.premium === 0) {
+    return res.status(403).send("This functionality is reserved for premium users");
+  }
+  next();
+};
 
 module.exports = checkPremium;
