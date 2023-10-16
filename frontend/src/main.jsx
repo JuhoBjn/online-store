813ea1de6f7f3760c9f7926af1643239f5714511
Juho Björkman <juho.bjorkman@tuni.fi>
2023-10-17 00:31:58 +0300

    Add routing #41

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,33 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import {
+  createBrowserRouter,
+  Navigate,
+  RouterProvider
+} from "react-router-dom";
+
 import App from "./App.jsx";
+import Authorization from "./pages/authorization/Authorization";
+
 import "./index.css";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />
+  },
+  {
+    path: "/auth",
+    element: <Authorization />
+  },
+  {
+    path: "*",
+    element: <Navigate to="/" replace="true" />
+  }
+]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
