2f5bda9ad98713e0a1c6488819fffd6d9267367a
Juho Björkman <juho.bjorkman@tuni.fi>
2023-10-20 01:20:04 +0300

    Add e2e test coverage #41

--- frontend/cypress.config.js
+++ frontend/cypress.config.js
@@ -1,11 +1,13 @@
 import { defineConfig } from "cypress";
+import task from "@cypress/code-coverage/task.js";
 
 export default defineConfig({
   e2e: {
     baseUrl: "http://localhost:5173",
     // eslint-disable-next-line no-unused-vars
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      task(on, config);
+      return config;
     }
   }
 });
