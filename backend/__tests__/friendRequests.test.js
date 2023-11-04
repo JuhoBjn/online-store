ff4609fe325e0b938c93ba3a75c693c851767014
Valtteri Huuskonen <valtteri.huuskonen@tuni.fi>
2023-11-04 22:44:35 +0200

    Add tests for get received friend requests endpoint

--- backend/__tests__/friendRequests.test.js
+++ backend/__tests__/friendRequests.test.js
@@ -170,3 +170,78 @@ describe("getSentFriendRequests", () => {
     expect(friendRequest.requested_friend_user_id).toBe(receiverUser.id);
   });
 });
+
+describe("getReceivedFriendRequests", () => {
+  let user;
+  let receiverUser;
+
+  const createUser = async (email, password) => {
+    const response = await request(app)
+      .post("/api/users/signup")
+      .send({ email, password });
+    return { id: response.body.id, email, token: response.body.token };
+  };
+
+  beforeEach(async () => {
+    user = await createUser("example-user@example.com", "Password123!");
+    receiverUser = await createUser(
+      "example-receiver-user@example.com",
+      "Password123!"
+    );
+  });
+
+  afterEach(async () => {
+    await promisePool.query("DELETE FROM users WHERE id in (?)", [
+      [user.id, receiverUser.id]
+    ]);
+  });
+
+  it("should return 400 if userid is not a valid UUID", async () => {
+    const response = await request(app)
+      .get("/api/users/invalid-uuid/friend-requests/received")
+      .auth(user.token, { type: "bearer" });
+
+    expect(response.status).toBe(400);
+    expect(response.body.message).toBe('"userid" must be a valid GUID');
+  });
+
+  it("should return 401 if userid does not match authenticated user", async () => {
+    const response = await request(app)
+      .get(
+        "/api/users/00000000-0000-0000-0000-000000000000/friend-requests/received"
+      )
+      .auth(user.token, { type: "bearer" });
+
+    expect(response.status).toBe(401);
+  });
+
+  it("should return 200 and the list of received friend requests if request is valid", async () => {
+    // get the list of sent friend requests before sending a friend request
+    const responseBefore = await request(app)
+      .get(`/api/users/${receiverUser.id}/friend-requests/received`)
+      .auth(receiverUser.token, { type: "bearer" });
+
+    expect(responseBefore.status).toBe(200);
+    expect(Array.isArray(responseBefore.body)).toBe(true);
+    expect(responseBefore.body.length).toBe(0);
+
+    // send a friend request
+    await request(app)
+      .post(`/api/users/${user.id}/friend-requests/${receiverUser.id}`)
+      .auth(user.token, { type: "bearer" });
+
+    // get the list of sent friend requests after sending a friend request
+    const responseAfter = await request(app)
+      .get(`/api/users/${receiverUser.id}/friend-requests/received`)
+      .auth(receiverUser.token, { type: "bearer" });
+
+    expect(responseAfter.status).toBe(200);
+    expect(Array.isArray(responseAfter.body)).toBe(true);
+    expect(responseAfter.body.length).toBe(1);
+
+    // check that the friend request is in the list
+    const friendRequest = responseAfter.body[0];
+    expect(friendRequest.requester_user_id).toBe(user.id);
+    expect(friendRequest.requested_friend_user_id).toBe(receiverUser.id);
+  });
+});
