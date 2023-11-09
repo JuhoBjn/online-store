834b961bd91fd7224f1b3477f0b593e635ccd78d
Juho Björkman <juho.bjorkman@tuni.fi>
2023-11-10 00:06:29 +0200

    Add tests for link to reset password page #72

--- frontend/cypress/e2e/auth.cy.js
+++ frontend/cypress/e2e/auth.cy.js
@@ -109,6 +109,23 @@ describe("The authentication page", () => {
     );
   });
 
+  it("should display a link to reset password upon wrong password", () => {
+    cy.login("larrys@test.com", "WrongP4ssword", true);
+
+    cy.get('[data-testid="reset-password-link"]').should("be.visible");
+    cy.get('[data-testid="reset-password-link"]').should(
+      "contain",
+      "Reset password"
+    );
+  });
+
+  it("should allow the user to navigate to the reset password page", () => {
+    cy.login("larrys@test.com", "WrongP4ssword", true);
+
+    cy.get('[data-testid="reset-password-link"]').click();
+    cy.url().should("be.equal", `${Cypress.config("baseUrl")}/reset-password`);
+  });
+
   it("should not allow a user to log in without an account", () => {
     cy.login("does.not@exist.com", "Noexisto123", true);
 
