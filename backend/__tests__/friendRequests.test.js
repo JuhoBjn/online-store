f7710b2a7f57972a6516764973fe8e4c9459d15a
Valtteri Huuskonen <valtteri.huuskonen@tuni.fi>
2023-11-11 00:11:58 +0200

    Add tests for acceptOrDenyFriendRequest endpoint

--- backend/__tests__/friendRequests.test.js
+++ backend/__tests__/friendRequests.test.js
@@ -5,9 +5,12 @@ const {
   it,
   expect,
   beforeEach,
-  afterEach
+  afterEach,
+  jest
 } = require("@jest/globals");
 const { promisePool } = require("../db/pool");
+const jwt = require("jsonwebtoken");
+const users = require("../models/users");
 
 describe("sendFriendRequest", () => {
   let senderUser;
@@ -246,5 +249,207 @@ describe("getReceivedFriendRequests", () => {
     const friendRequest = responseAfter.body[0];
     expect(friendRequest.requester_user_id).toBe(user.id);
     expect(friendRequest.requested_friend_user_id).toBe(receiverUser.id);
+  });
+});
+
+describe("acceptOrDenyFriendRequest", () => {
+  let friendRequestId;
+  let token;
+  beforeEach(async () => {
+    // Create two users for testing.
+    await promisePool.query(
+      "INSERT INTO users (id, email, password, role_id) VALUES (?, ?, ?, ?), (?, ?, ?, ?)",
+      [
+        "F31FF197-88D3-4F45-A566-B8E7DC6BAF42",
+        "exampleuser132213@example.com",
+        "imagine a hashed password here",
+        1,
+        "7BB4A1C9-187B-4A68-885C-3BE6B1828B6B",
+        "exampleuser543534@example.com",
+        "imagine a hashed password here",
+        1
+      ]
+    );
+
+    const friendRequestQuery = await promisePool.query(
+      "INSERT INTO friend_requests (requester_user_id, requested_friend_user_id) VALUES (?, ?)",
+      [
+        "F31FF197-88D3-4F45-A566-B8E7DC6BAF42",
+        "7BB4A1C9-187B-4A68-885C-3BE6B1828B6B"
+      ]
+    );
+    friendRequestId = friendRequestQuery[0].insertId;
+
+    // Get the token for the requested user
+    token = await jwt.sign(
+      { id: "7BB4A1C9-187B-4A68-885C-3BE6B1828B6B", role_id: 1 },
+      process.env.JWT_KEY,
+      { expiresIn: "1h" }
+    );
+  });
+
+  afterEach(async () => {
+    await promisePool.query("DELETE FROM users WHERE id IN (?)", [
+      [
+        "F31FF197-88D3-4F45-A566-B8E7DC6BAF42",
+        "7BB4A1C9-187B-4A68-885C-3BE6B1828B6B"
+      ]
+    ]);
+  });
+
+  it("should accept a friend request", async () => {
+    const response = await request(app)
+      .put(
+        `/api/users/7BB4A1C9-187B-4A68-885C-3BE6B1828B6B/friend-requests/${friendRequestId}`
+      )
+      .send({ status: "accepted" })
+      .auth(token, { type: "bearer" });
+
+    expect(response.status).toBe(201);
+    expect(response.body.message).toBe(
+      "Friend request accepted and friendship created"
+    );
+  });
+
+  it("should deny a friend request", async () => {
+    const response = await request(app)
+      .put(
+        `/api/users/7BB4A1C9-187B-4A68-885C-3BE6B1828B6B/friend-requests/${friendRequestId}`
+      )
+      .send({ status: "denied" })
+      .auth(token, { type: "bearer" });
+
+    expect(response.status).toBe(200);
+    expect(response.body.message).toBe("Friend request denied");
+  });
+
+  it("should return 400 for invalid status", async () => {
+    const response = await request(app)
+      .put(
+        `/api/users/7BB4A1C9-187B-4A68-885C-3BE6B1828B6B/friend-requests/${friendRequestId}`
+      )
+      .send({ status: "invalid" })
+      .auth(token, { type: "bearer" });
+
+    expect(response.status).toBe(400);
+    expect(response.body.message).toBe(
+      '"status" must be one of [accepted, denied]'
+    );
+  });
+
+  it("should return 400 for invalid user ID", async () => {
+    const response = await request(app)
+      .put(`/api/users/invalid-user-id/friend-requests/${friendRequestId}`)
+      .send({ status: "accepted" })
+      .auth(token, { type: "bearer" });
+
+    expect(response.status).toBe(400);
+    expect(response.body.message).toBe('"userid" must be a valid GUID');
+  });
+
+  it("should return 403 for unauthorized user", async () => {
+    const response = await request(app)
+      .put(
+        `/api/users/F31FF197-88D3-4F45-A566-B8E7DC6BAF42/friend-requests/${friendRequestId}`
+      )
+      .send({ status: "accepted" })
+      .auth(token, { type: "bearer" }); // token is for a different user
+
+    expect(response.status).toBe(403);
+    expect(response.body.message).toBe("Unauthorized");
+  });
+
+  it("should return 403 for valid user ID, that is not a pending friend request", async () => {
+    // Generate a token for a user that does not have a pending friend request
+    const token_ddff = await jwt.sign(
+      { id: "ddfffcd7-983c-4f83-b998-884c36bea194", role_id: 1 },
+      process.env.JWT_KEY,
+      { expiresIn: "1h" }
+    );
+
+    const response = await request(app)
+      .put(
+        `/api/users/ddfffcd7-983c-4f83-b998-884c36bea194/friend-requests/${friendRequestId}`
+      )
+      .send({ status: "accepted" })
+      .auth(token_ddff, { type: "bearer" });
+
+    expect(response.status).toBe(403);
+    expect(response.body.message).toBe("Unauthorized");
+  });
+
+  it("should return 400 for non-existing friend request ID", async () => {
+    const response = await request(app)
+      .put(
+        `/api/users/7BB4A1C9-187B-4A68-885C-3BE6B1828B6B/friend-requests/999`
+      )
+      .send({ status: "accepted" })
+      .auth(token, { type: "bearer" });
+
+    expect(response.status).toBe(400);
+    expect(response.body.message).toBe(
+      "No friend request exists with given friendRequestId"
+    );
+  });
+
+  it("should return 500 for internal error during friend request acceptance", async () => {
+    // Mock an internal error during the acceptance process
+    jest.spyOn(users, "acceptFriendRequest").mockImplementation(() => {
+      throw new Error("Internal error");
+    });
+
+    const response = await request(app)
+      .put(
+        `/api/users/7BB4A1C9-187B-4A68-885C-3BE6B1828B6B/friend-requests/${friendRequestId}`
+      )
+      .send({ status: "accepted" })
+      .auth(token, { type: "bearer" });
+
+    expect(response.status).toBe(500);
+    expect(response.body.message).toBe("Internal error");
+
+    // Restore the original function to avoid side effects on other tests
+    jest.spyOn(users, "acceptFriendRequest").mockRestore();
+  });
+
+  it("should return 500 for internal error during friend request deny", async () => {
+    // Mock an internal error during the acceptance process
+    jest.spyOn(users, "updateFriendRequest").mockImplementation(() => {
+      throw new Error("Internal error");
+    });
+
+    const response = await request(app)
+      .put(
+        `/api/users/7BB4A1C9-187B-4A68-885C-3BE6B1828B6B/friend-requests/${friendRequestId}`
+      )
+      .send({ status: "denied" })
+      .auth(token, { type: "bearer" });
+
+    expect(response.status).toBe(500);
+    expect(response.body.message).toBe("Internal error");
+
+    // Restore the original function to avoid side effects on other tests
+    jest.spyOn(users, "updateFriendRequest").mockRestore();
+  });
+
+  it("should return 400 if friend request exists but is not pending", async () => {
+    // Deny the friend request before so that it is no longer pending
+    await request(app)
+      .put(
+        `/api/users/7BB4A1C9-187B-4A68-885C-3BE6B1828B6B/friend-requests/${friendRequestId}`
+      )
+      .send({ status: "denied" })
+      .auth(token, { type: "bearer" });
+
+    // Try to accept the friend request
+    const response = await request(app)
+      .put(
+        `/api/users/7BB4A1C9-187B-4A68-885C-3BE6B1828B6B/friend-requests/${friendRequestId}`
+      )
+      .send({ status: "accepted" })
+      .auth(token, { type: "bearer" });
+
+    expect(response.status).toBe(400);
+    expect(response.body.message).toBe("Friend request is not pending");
   });
 });
