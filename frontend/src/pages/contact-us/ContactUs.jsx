2c766b69fa413a83f7882ede7d31fc00a22afaba
Juho Björkman <juho.bjorkman@tuni.fi>
2023-11-18 23:58:19 +0200

    Add a contact form #97

--- frontend/src/pages/contact-us/ContactUs.jsx
+++ frontend/src/pages/contact-us/ContactUs.jsx
@@ -1,7 +1,67 @@
+import { useRef, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
+import Button from "../../components/button/Button";
+
 import "./ContactUs.css";
 
 const ContactUs = () => {
-  return <h1>Contact us</h1>;
+  const emailRef = useRef("");
+  const messageRef = useRef("");
+  const navigate = useNavigate();
+
+  const submitMessageHandler = (event) => {
+    event.preventDefault();
+
+    console.log(
+      `Email: ${emailRef.current.value}, message: ${messageRef.current.value}`
+    );
+  };
+
+  useEffect(() => {
+    const currentUser = JSON.parse(localStorage.getItem("currentUser"));
+    if (currentUser?.token) {
+      navigate("/");
+    }
+  }, [navigate]);
+
+  return (
+    <div className="contact-us-page">
+      <div className="contact-us-form-container">
+        <h2>Contact Form</h2>
+        <form
+          data-testid="contact-us-form"
+          id="contact-us-form"
+          onSubmit={submitMessageHandler}
+        >
+          <label htmlFor="email-input" hidden>
+            Enter your email here
+          </label>
+          <input
+            id="email-input"
+            type="email"
+            placeholder="Your email"
+            ref={emailRef}
+            required
+          />
+          <label htmlFor="message-input" hidden>
+            Enter your message here
+          </label>
+          <textarea
+            id="message-input"
+            placeholder="Leave your message here"
+            ref={messageRef}
+            autoComplete="on"
+            rows="5"
+            required
+          />
+          <div className="contact-us-form-button-container">
+            <Button type="confirm">Send message</Button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default ContactUs;
