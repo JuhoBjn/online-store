1b85961ec0e8bd8d8bc95bb5d90dbc6b3136efc6
Juho Björkman <juho.bjorkman@tuni.fi>
2023-11-18 22:41:14 +0200

    Escape apostrophes in JSX, remove unused auth context #97

--- frontend/src/pages/frontpage/Frontpage.jsx
+++ frontend/src/pages/frontpage/Frontpage.jsx
@@ -1,14 +1,12 @@
 import { useNavigate } from "react-router-dom";
-import { useEffect, useContext } from "react";
+import { useEffect } from "react";
 
 import Button from "../../components/button/Button";
-import { AuthContext } from "../../utils/AuthContext";
 
 import "./Frontpage.css";
 
 const Frontpage = () => {
   const navigate = useNavigate();
-  const authContext = useContext(AuthContext);
 
   const navigateToAuthHandler = () => {
     navigate("/auth");
@@ -20,18 +18,18 @@ const Frontpage = () => {
     if (currentUser?.token) {
       navigate("/");
     }
-  }, []);
+  }, [navigate]);
 
   return (
     <div className="frontpage">
       <div className="frontpage-content">
         <h2 id="frontpage-title">What is GoldenAge?</h2>
         <p>
-          GoldenAge is more than a community; it's a celebration of life's
-          richness and the pursuit of vibrant connections. We are a dedicated
-          platform designed to enhance the lives of seniors through a diverse
-          array of engaging activities, cultural experiences, and supportive
-          connections.
+          GoldenAge is more than a community; it&apos;s a celebration of
+          life&apos;s richness and the pursuit of vibrant connections. We are a
+          dedicated platform designed to enhance the lives of seniors through a
+          diverse array of engaging activities, cultural experiences, and
+          supportive connections.
         </p>
       </div>
       <Button id="join-button" type="confirm" onClick={navigateToAuthHandler}>
