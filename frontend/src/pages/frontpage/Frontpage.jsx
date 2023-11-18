c37caa07c0a3cd9a3b400450bfa53bae758a8e83
Juho Björkman <juho.bjorkman@tuni.fi>
2023-11-18 20:49:36 +0200

    Add front page content and style #97

--- frontend/src/pages/frontpage/Frontpage.jsx
+++ frontend/src/pages/frontpage/Frontpage.jsx
@@ -1,7 +1,33 @@
+import { useNavigate } from "react-router-dom";
+
+import Button from "../../components/button/Button";
+
 import "./Frontpage.css";
 
 const Frontpage = () => {
-  return <h1>Front page</h1>;
+  const navigate = useNavigate();
+
+  const navigateToAuthHandler = () => {
+    navigate("/auth");
+  };
+
+  return (
+    <div className="frontpage">
+      <div className="frontpage-content">
+        <h2 id="frontpage-title">What is GoldenAge?</h2>
+        <p>
+          GoldenAge is more than a community; it's a celebration of life's
+          richness and the pursuit of vibrant connections. We are a dedicated
+          platform designed to enhance the lives of seniors through a diverse
+          array of engaging activities, cultural experiences, and supportive
+          connections.
+        </p>
+      </div>
+      <Button id="join-button" type="confirm" onClick={navigateToAuthHandler}>
+        Join
+      </Button>
+    </div>
+  );
 };
 
 export default Frontpage;
