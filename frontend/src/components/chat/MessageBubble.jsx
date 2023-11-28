7f3b470a6ebc76f7f7157ad15c7b84e75a0528a1
Valtteri Huuskonen <valtteri.huuskonen@tuni.fi>
2023-11-28 03:26:22 +0200

    Refactor MessageBubble component order & add time formatting utility function

--- frontend/src/components/chat/MessageBubble.jsx
+++ frontend/src/components/chat/MessageBubble.jsx
@@ -1,19 +1,25 @@
+import "./MessageBubble.css";
+import formatTime from "./timeFormat";
+
 const MessageBubble = ({ message, isMine, isGroupChat }) => {
   const { message: textContent, sentAt, name } = message;
 
   return (
-    <div
-      className={`chat-message-bubble ${
-        isMine ? "chat-message-bubble--mine" : "chat-message-bubble--theirs"
-      }`}
-    >
-      <div className="chat-message-bubble__content">
-        {isGroupChat && !isMine && (
-          <div className="chat-message-bubble__name">{name}</div>
-        )}
-
-        <div className="chat-message-bubble__text">{textContent}</div>
-        <div className="chat-message-bubble__time">{sentAt}</div>
+    <div className="chat-message-bubble-container">
+      <div
+        className={`chat-message-bubble ${
+          isMine ? "chat-message-bubble--mine" : "chat-message-bubble--theirs"
+        }`}
+      >
+        <div>
+          {isGroupChat && !isMine && (
+            <div className="chat-message-bubble__name">{name}</div>
+          )}
+          <div className="chat-message-bubble__content">
+            <div className="chat-message-bubble__text">{textContent}</div>
+          </div>
+          <div className="chat-message-bubble__time">{formatTime(sentAt)}</div>
+        </div>
       </div>
     </div>
   );
