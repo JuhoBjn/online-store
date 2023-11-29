3394e03c67fccb238565f5395a2a5db57f264539
Juho Björkman <juho.bjorkman@tuni.fi>
2023-11-29 22:42:29 +0200

    Add horizontal rule for better separation of articles #121

--- frontend/src/components/news/NewsArticle.jsx
+++ frontend/src/components/news/NewsArticle.jsx
@@ -4,28 +4,31 @@ import "./NewsArticle.css";
 
 const NewsArticle = ({ id, headline, body, imageUrl, link }) => {
   return (
-    <div
-      data-testid="news-article-container"
-      className="news-article-container"
-    >
-      <div
-        className={`news-article-image-container ${
-          id % 2 == 0 ? "right" : "left"
-        }`}
+    <>
+      <article
+        data-testid="news-article-container"
+        className="news-article-container"
       >
-        <img
-          data-testid="news-article-image"
-          src={imageUrl}
-          alt={`Article image: ${headline}`}
-        />
-      </div>
-      <div className="news-article-content">
-        <Link to={link} data-testid="news-article-headline">
-          {headline}
-        </Link>
-        <p data-testid="news-article-body">{body}</p>
-      </div>
-    </div>
+        <div
+          className={`news-article-image-container ${
+            id % 2 == 0 ? "right" : "left"
+          }`}
+        >
+          <img
+            data-testid="news-article-image"
+            src={imageUrl}
+            alt={`Article image: ${headline}`}
+          />
+        </div>
+        <div className="news-article-content">
+          <Link to={link} data-testid="news-article-headline">
+            {headline}
+          </Link>
+          <p data-testid="news-article-body">{body}</p>
+        </div>
+      </article>
+      <hr />
+    </>
   );
 };
 
