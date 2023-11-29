320c1395af64c17670dddc4236ea393bafd24b26
Juho Björkman <juho.bjorkman@tuni.fi>
2023-11-29 22:45:45 +0200

    Add news list to news page #121

--- frontend/src/pages/news/NewsLoader.js
+++ frontend/src/pages/news/NewsLoader.js
@@ -1,9 +1,13 @@
 import { redirect } from "react-router-dom";
 
+import { fetchAllNews } from "../../utils/NewsAPI";
+
 export const NewsLoader = () => {
   const storedUser = JSON.parse(localStorage.getItem("currentUser"));
+
   if (!storedUser?.token) {
     return redirect("/frontpage");
   }
-  return true;
+
+  return fetchAllNews(storedUser.token);
 };
