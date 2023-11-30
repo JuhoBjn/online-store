5990cc1e6f47d27a7e342fee19beed505f7188b7
Juho Björkman <juho.bjorkman@tuni.fi>
2023-12-01 00:48:18 +0200

    Placeholder caretaker page #118

---
+++ frontend/src/pages/caretaker/Caretaker.jsx
@@ -0,0 +1,11 @@
+import "./Caretaker.css";
+
+const Caretaker = () => {
+  return (
+    <div className="caretaker-page">
+      <h1>Caretaker page</h1>
+    </div>
+  );
+};
+
+export default Caretaker;
