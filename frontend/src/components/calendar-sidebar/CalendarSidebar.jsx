5b0e8187344ad08223f971a0b6ebd58250ff9c22
Juho Björkman <juho.bjorkman@tuni.fi>
2023-12-03 22:21:12 +0200

    Update frontend dependencies to fix vulnerability #122

--- frontend/src/components/calendar-sidebar/CalendarSidebar.jsx
+++ frontend/src/components/calendar-sidebar/CalendarSidebar.jsx
@@ -1,7 +1,6 @@
 import { useState, useEffect } from "react";
-import Calendar from "react-calendar";
 
-import "react-calendar/dist/Calendar.css";
+
 import "./CalendarSidebar.css";
 
 const CalendarSidebar = ({ calendar }) => {
