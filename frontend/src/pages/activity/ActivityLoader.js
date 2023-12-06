bd438a8715084202ca6c6ddb15762eb73698f4cc
Juho Björkman <juho.bjorkman@tuni.fi>
2023-12-06 02:15:36 +0200

    Remove import of image from assets #120

--- frontend/src/pages/activity/ActivityLoader.js
+++ frontend/src/pages/activity/ActivityLoader.js
@@ -2,8 +2,6 @@ import { redirect } from "react-router-dom";
 
 import { fetchSingleActivity } from "../../utils/ActivityAPI";
 
-import ElatedPensioner from "../../assets/Elated_pensioner.jpg";
-
 export const ActivityLoader = async ({ params }) => {
   const storedUser = JSON.parse(localStorage.getItem("currentUser"));
 
@@ -11,10 +9,5 @@ export const ActivityLoader = async ({ params }) => {
     return redirect("/frontpage");
   }
 
-  const activity = await fetchSingleActivity(
-    params.activityId,
-    storedUser.token
-  );
-  activity.image_url = ElatedPensioner;
-  return activity;
+  return await fetchSingleActivity(params.activityId, storedUser.token);
 };
