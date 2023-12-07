eb29b4fc5ad176f6b3e6c885d248504ddfda4159
Juho Björkman <juho.bjorkman@tuni.fi>
2023-12-07 20:09:27 +0200

    Add headings to about us page #137

--- frontend/src/pages/about-us/AboutUs.jsx
+++ frontend/src/pages/about-us/AboutUs.jsx
@@ -22,7 +22,10 @@ const AboutUs = () => {
           data-testid="about-us-content-left"
           className="about-us-content-left"
         >
-          <p data-testid="about-us-paragraph1">
+          <h2 data-testid="about-us-heading-1" className="about-us-heading">
+            Welcome to GoldenAge
+          </h2>
+          <p data-testid="about-us-paragraph1" className="about-us-paragraph">
             Welcome to GoldenAge, where we believe in making every phase of life
             vibrant, engaging, and fulfilling. We understand the challenges that
             elderly individuals face, and we&apos;re here to transform their
@@ -30,7 +33,10 @@ const AboutUs = () => {
             myriad of activities, from lively parties to the pursuit of exciting
             new hobbies and the chance to make new friends.
           </p>
-          <p data-testid="about-us-paragraph2">
+          <h2 data-testid="about-us-heading-2" className="about-us-heading">
+            Our Mission
+          </h2>
+          <p data-testid="about-us-paragraph2" className="about-us-paragraph">
             Our mission is clear: to reduce loneliness, promote an active and
             happy lifestyle, and ultimately improve the quality of life and
             mental well-being of the elderly. We are dedicated to creating a
