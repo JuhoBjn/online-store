0cee4073e101383bb0465ae0ad20bd5859fa6734
Valtteri Huuskonen <valtteri.huuskonen@tuni.fi>
2023-12-10 22:46:14 +0200

    Revert "more port fix"
    This reverts commit ac777ccdb5a5f4d8206d800eb8bab36df34d36fd.

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 const { httpServer } = require("./app");
 
-const port = process.env.API_PORT || 5000;
+const port = process.env.PORT || 5000;
 
 httpServer.listen(port, () => {
   console.log(`Backend is listening on port ${port}`);
